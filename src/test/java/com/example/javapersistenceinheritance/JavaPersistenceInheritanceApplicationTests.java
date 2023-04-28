3ffd2e67270748bfd061eac639a229a7e3c85e8d
yunus.ustun <yunus.ustun@tubitak.gov.tr>
2023-04-28 18:04:55 +0300

    first commit

---
+++ src/test/java/com/example/javapersistenceinheritance/JavaPersistenceInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javapersistenceinheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPersistenceInheritanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
